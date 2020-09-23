257a5006e2f722564f6131d3f2dca541e50f008a
Will <doggy.huang@gmail.com>
2020-09-23 09:51:45 +0800

    加入 /Courses2 頁面

---
+++ WebApplication1/Controllers/Courses2Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Courses2Controller : Controller
+    {
+        ContosoUniversityEntities db = new ContosoUniversityEntities();
+
+        public ActionResult Index()
+        {
+            var data = db.Course.ToList();
+
+            return View(data);
+        }
+    }
+}

d89a0e5af9d72d66ac905cf5c28b29913b037289
Will <doggy.huang@gmail.com>
2020-09-23 15:51:33 +0800

    實作模型驗證

--- WebApplication1/Models/Course.Partial.cs
+++ WebApplication1/Models/Course.Partial.cs
@@ -7,8 +7,23 @@ using System.Web;
 namespace WebApplication1.Models
 {
     [MetadataType(typeof(CourseMetadata))]
-    public partial class Course
+    public partial class Course : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Title.StartsWith("A") && this.Credits < 3)
+            {
+                yield return new ValidationResult("天龍國課程必須 Credits 大於等於 3", new[] { "Title" });
+            }
+
+            if (!this.Title.StartsWith("A") && this.Credits < 2)
+            {
+                yield return new ValidationResult("所有課程必須 Credits 大於等於 2", new[] { "Title" });
+            }
+
+            //yield return ValidationResult.Success;
+        }
+
         private class CourseMetadata
         {
             public int CourseID { get; set; }

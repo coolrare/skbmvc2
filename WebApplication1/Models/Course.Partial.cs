35a7f9f6d4116f931aaea1cbfe7c5097bc9172b6
Will <doggy.huang@gmail.com>
2020-09-23 14:03:02 +0800

    加入 MetadataType

---
+++ WebApplication1/Models/Course.Partial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [MetadataType(typeof(CourseMetadata))]
+    public partial class Course
+    {
+        private class CourseMetadata
+        {
+            public int CourseID { get; set; }
+            [Required]
+            [MaxLength(30)]
+            public string Title { get; set; }
+            [Required]
+            [Range(1, 5)]
+            public int Credits { get; set; }
+            public int DepartmentID { get; set; }
+            [Required]
+            public System.DateTime CreatedOn { get; set; }
+        }
+    }
+}
